fce12315c5ead1153e2044d3e33a87110ffbcaef
Kalea Chu <kaleachu@berkeley.edu>
2021-03-26 20:22:47 -0700

    modified progress bars, added toggle switch, conditional discussion post component

---
+++ client/src/pages/proposalDetails/DiscussionPost.js
@@ -0,0 +1,34 @@
+import './DiscussionPost.css';
+
+function ConditionalUserNameRender(isAdmin, userName) {
+    if (isAdmin) {
+      return (
+        <div>
+          {userName}
+        </div>);
+    } else {
+      return <div>Anon User</div>;
+    }
+  }
+
+function DiscussionPost(props) {
+    return (
+      <div className="post">
+          <div className="user">
+            {ConditionalUserNameRender(props.isAdmin, props.userName)}
+          </div>
+          <div className="text">
+            <div className="content">
+                {props.text}
+            </div>
+            
+            <div className="timestamp">
+              posted: {props.time}
+          </div>
+          </div>
+          
+      </div>
+    );
+  }
+
+  export default DiscussionPost;
--- client/src/pages/proposalDetails/ProposalDetails.js
+++ client/src/pages/proposalDetails/ProposalDetails.js
@@ -1,15 +1,35 @@
 import './ProposalDetails.css';
 import ProgressBar from 'react-bootstrap/ProgressBar';
+import DiscussionPost from './DiscussionPost.js';
 
 var IS_ADMIN = true;
 var DEADLINE = '3/5/21';
 var AMOUNT = 14360;
-var PERCENT_VOTED = 60;
-var PERCENT_YES = 20;
+
+var TOTAL_MEMBERS = 60;
+var AMOUNT_VOTED = 84;
+var PERCENT_YES = 50;
+var PERCENT_NO = 20;
+var PERCENT_UNVOTED = 30;
+
+var DUMMY_USER1 = 'kalea';
+var DUMMY_USER2 = 'jorge';
+
+
+
+var DUMMY_TEXT1 = 'i like this';
+var DUMMY_TEXT2 = 'i dont like this';
+
+var DUMMY_TIME1 = '3/26/21 3:12pm';
+var DUMMY_TIME2 = '3/27/21 10:09pm';
+
+var WHITESPACE = <div>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</div>;
+
 var DUMMY_DESCRIPTION = `The Earth Action Initiative will organize a variety of
 events and actions that build community, leverage existing
 efforts, inspire new plans, and find innovative ways to
-execute these actions through effective communication.`
+execute these actions through effective communication.`;
+
 
 /** Takes in a number and converts it to a dollar amount string w/ commas
 placed appropriately (every 3 spaces); does not include dollar sign
@@ -22,11 +42,43 @@ function ConditionalProposalRender(isAdmin) {
   if (isAdmin) {
     return (
       <div className='progressBarsFrame'>
-        <ProgressBar className='progressBarPercentVoted' variant='info' now={PERCENT_VOTED} label={`${PERCENT_VOTED}% Voted`}/>
-        <ProgressBar className='progressBarPercentYes' variant='success' now={PERCENT_YES} label={`${PERCENT_YES}% YES`}/>
+        <ProgressBar className='progressBarYesNoUnvoted'>
+          <ProgressBar className='progressBarPercentYes' variant='success' now={PERCENT_YES} label={`${PERCENT_YES}% YES`} key={1}/>
+          <ProgressBar className='progressBarPercentNo' variant='danger' now={PERCENT_NO} label={`${PERCENT_NO}% NO`} key={2}/>
+          <ProgressBar className='progressBarPercentUnvoted' variant='customProgressBarUnvoted' now={PERCENT_UNVOTED} label={`${PERCENT_UNVOTED}% UNVOTED`} key={3}/>
+        </ProgressBar>
+        <div className='amountVotedLabel'>
+          20/30 Voted
+        </div>
       </div>);
   } else {
     return <div>voting buttons</div>;
+  }
+}
+
+/** we need this to set a global var? with useState? */
+function ConditionalToggleRender(isAdmin) {
+  if (isAdmin) {
+    return (
+      // <div className="toggle">
+      //   <input type="checkbox" />
+      //   <div className="circle"></div>
+      //   <div className="toggleLeft">vote</div> <div className="toggleRight">progress</div>
+      // </div>
+        <div className="switch_container">
+
+          <label class="switch">
+            <input type="checkbox" name= "" id=""/>
+            <span class="slider round"></span>
+          </label>
+
+          <div className="labels">
+            <div className="toggleLeft">vote&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</div> 
+            <div className="toggleRight">progress</div>
+          </div>
+          
+        </div>
+    )
   }
 }
 
@@ -46,10 +98,17 @@ function ProposalDetails() {
           </div>
           <div className='proposalConditional'>
             {ConditionalProposalRender(IS_ADMIN)}
+            {ConditionalToggleRender(IS_ADMIN)}
           </div>
         </div>
         <div className="discussion">
-          discussion
+        <div className="discussionTitle">
+              Discussion
+              <hr className="underline"></hr>
+
+              <DiscussionPost isAdmin={IS_ADMIN} userName={DUMMY_USER1} text={DUMMY_TEXT1} time={DUMMY_TIME1}/>
+              <DiscussionPost isAdmin={IS_ADMIN} userName={DUMMY_USER2} text={DUMMY_TEXT2} time={DUMMY_TIME2}/>
+            </div>
         </div>
       </div>
     );
