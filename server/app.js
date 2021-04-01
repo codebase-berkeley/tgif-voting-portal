104541a6a9ecef048fdc810518a2a45a9ac5e840
Ryan Zhao <ryanzhao@berkeley.edu>
2021-03-31 21:42:09 -0700

    updated README + eslint

--- server/app.js
+++ server/app.js
@@ -9,11 +9,11 @@ app.get('/', (req, res) => {
   res.send('Hello World');
 });
 
-//Example endpoint that sends a try-catch wrapped query to the database
+// Example endpoint that sends a try-catch wrapped query to the database
 app.get('/test_db', async (req, res) => {
   try {
     const query = await db.query(
-      'SELECT * FROM users;'
+      'SELECT * FROM users;',
     );
     res.send(query.rows);
   } catch (error) {
@@ -21,12 +21,12 @@ app.get('/test_db', async (req, res) => {
   }
 });
 
-//Example endpoint that sends a try-catch wrapped query to the database
+// Example endpoint that sends a try-catch wrapped query to the database
 app.get('/test_db2', async (req, res) => {
-  let name = "Warren";
+  const name = 'Warren';
   try {
     const query = await db.query(
-      "SELECT * FROM users WHERE username=$1;", [name]
+      'SELECT * FROM users WHERE username=$1;', [name],
     );
     res.send(query.rows);
   } catch (error) {
@@ -34,13 +34,13 @@ app.get('/test_db2', async (req, res) => {
   }
 });
 
-//Example endpoint that sends a try-catch wrapped query to the database
+// Example endpoint that sends a try-catch wrapped query to the database
 app.post('/test_db3', async (req, res) => {
   try {
-    const query = await db.query(
-      "INSERT INTO users (id, is_admin, username) VALUES ($1, $2, $3);", ['5', true, 'SuperAdmin']
+    await db.query(
+      'INSERT INTO users (id, is_admin, username) VALUES ($1, $2, $3);', ['5', true, 'SuperAdmin'],
     );
-    res.send("Success");
+    res.send('Success');
   } catch (error) {
     console.log(error.stack);
   }
