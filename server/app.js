bfa28e345aec1074cebb04b411f4b2845504984b
Jorge Morales <jorge.morales@berkeley.edu>
2021-04-25 13:25:53 -0700

    Deleted a comment

--- server/app.js
+++ server/app.js
@@ -8,7 +8,7 @@ require('dotenv').config();
 const corsOptions = {
   origin: 'http://localhost:3000',
   credentials: true,
-  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+  optionsSuccessStatus: 200,
 };
 
 const app = express();
