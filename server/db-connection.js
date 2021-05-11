df789051b34c4fef45e16ccf2488bad3e2835ec2
Warren <warrenwwang@berkeley.edu>
2021-05-11 13:45:50 -0700

    fixing db-connection that i broke

--- server/db-connection.js
+++ server/db-connection.js
@@ -1,6 +1,6 @@
 const { Pool } = require('pg');
 const poolConfig = process.env.DATABASE_URL
-  ? { connectionString: poolConfig }
+  ? { connectionString: process.env.DATABASE_URL }
   : { database: 'tgif' };
 
 const pool = new Pool(poolConfig);
